9b0a2808cf639bdc72b0c1fb19d78a99239b8ae1
xialonglee <li.xialong@zte.com.cn>
2017-06-01 01:22:07 +0800

    fix bug

--- main.go
+++ main.go
@@ -36,7 +36,8 @@ type ValidityOpts struct {
 		   } `yaml: args_check`
 }
 
-const relative_prefix_path = "/home/k8s_kdt/kdt/"
+//const relative_prefix_path = "/home/k8s_kdt/kdt/"
+const relative_prefix_path = "D:\\go_src\\contrib\\ansible\\"
 
 var Conf_paths = config_paths{
 	flannel_conf_path:        "roles/flannel/defaults/main.yaml",
@@ -47,27 +48,27 @@ var Conf_paths = config_paths{
 
 func main() {
 	paths := [2]string{relative_prefix_path + Conf_paths.validity_check_conf_path, relative_prefix_path + Conf_paths.flannel_conf_path}
-	CompareArgs(paths, ValidityOpts{}, DefaultOpts{}, "flannel")
+	fmt.Println(CompareArgs(paths, ValidityOpts{}, DefaultOpts{}, "flannel"))
 
 }
 
 func CompareArgs(file_path [2]string, validity_opts ValidityOpts, default_opts DefaultOpts, component_name string) (bool bool, err error) {
 	fmt.Printf("Check %s args in path %s and path %s.\n", component_name, file_path[0], file_path[1])
-	filed_name := strings.Title(component_name + "_opts")
-	validity_value, default_value, err := yaml_unmarshal(file_path, validity_opts, default_opts, filed_name)
+	field_name := strings.Title(component_name + "_opts")
+	validity_value, default_value, err := yaml_unmarshal(file_path, validity_opts, default_opts, field_name)
 	if err != nil {
 		return false, err
 	}
 
-	fmt.Printf("validity_value is %v", validity_value)
-	fmt.Printf("default_value is %v", default_value)
+	fmt.Printf("validity_value is %v\n", validity_value)
+	fmt.Printf("default_value is %v\n", default_value)
 	if isInclude(validity_value, default_value) && isInclude(default_value, validity_value) {
 		return true, nil
 	}
 	return false, nil
 }
 
-func yaml_unmarshal(file_path [2]string, validity_opts ValidityOpts, default_opts DefaultOpts, filed_name string) ([]string, []string, error) {
+func yaml_unmarshal(file_path [2]string, validity_opts ValidityOpts, default_opts DefaultOpts, field_name string) ([]string, []string, error) {
 	data, err := ioutil.ReadFile(file_path[0])
 	if err != nil {
 		return []string{}, []string{}, fmt.Errorf("Reading config file failed: %v", err)
@@ -80,24 +81,19 @@ func yaml_unmarshal(file_path [2]string, validity_opts ValidityOpts, default_opt
 	}
 	yaml.Unmarshal(data, &default_opts)
 
-	validity_value := reflect.ValueOf(validity_opts).FieldByName("Args_check").FieldByName(filed_name)
-	default_value:= reflect.ValueOf(default_opts).FieldByName(filed_name)
-	val_inf := reflect.MakeSlice(validity_value.Type(),validity_value.Len(),validity_value.Len()).Interface()
-	def_inf := reflect.MakeSlice(default_value.Type(),default_value.Len(),default_value.Len()).Interface()
-	val, _ := val_inf.([]string)
-	def, _ := def_inf.([]string)
-	fmt.Printf("%v",val)
-	fmt.Printf("%v",def)
-
-	return []string,[]string, nil
+	validity_value := reflect.ValueOf(validity_opts).FieldByName("Args_check").FieldByName(field_name).Interface()
+	default_value := reflect.ValueOf(default_opts).FieldByName(field_name).Interface()
+	val, _ := validity_value.([]string)
+	def, _ := default_value.([]string)
+	return val, def, nil
 }
 
 func isInclude(args0, args1 []string) bool {
-	regx := regexp.MustCompile(`\s*-+([^\s=]+).*`)
-	include := false
+	regx := regexp.MustCompile(`s*-+([^s=]+).*`)
 	for _, arg0 := range args0 {
+		include := false
 		for _, arg1 := range args1 {
-			if regx.FindString(arg0) == regx.FindString(arg1) {
+			if regx.FindStringSubmatch(arg0)[1] == regx.FindStringSubmatch(arg1)[1] {
 				include = true
 				break
 			}
@@ -106,5 +102,5 @@ func isInclude(args0, args1 []string) bool {
 			return include
 		}
 	}
-	return include
+	return true
 }
